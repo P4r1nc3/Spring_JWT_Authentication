c6148604e0f6a2c7d58f39e0831950080a9d7fd3
parince <konrad.tupta@gmail.com>
2023-03-05 13:26:19 +0100

    Added role repository

---
+++ src/main/java/com/springdemo/jwt/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.springdemo.jwt.repository;
+
+
+import com.springdemo.jwt.entity.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, String> {
+
+}
